87c935639bda2cb968907def43175789fb9ff900
maryavasylieva <miheta.laurelin@gmail.com>
2020-02-23 21:40:29 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Root from "./Components/Root/Root";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-serviceWorker.unregister();
+ReactDOM.render(<Root />, document.getElementById("root"));
