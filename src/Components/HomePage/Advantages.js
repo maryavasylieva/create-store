4b91e1ace26e8672097a4bcc67720e5ae1518a18
maryavasylieva <miheta.laurelin@gmail.com>
2020-03-12 22:45:58 +0200

    adding svg icons in advantages component

--- src/Components/HomePage/Advantages.js
+++ src/Components/HomePage/Advantages.js
@@ -5,47 +5,55 @@ import css from "./HomePage.module.css";
 
 const Advantages = () => {
   return (
-    <Section className={css.section}>
-      <Delivery>
+    <ul className={css.section}>
+      <li className = {css.list}>
+          <div  className={css.delivery}>
         <SubTitle>Доставка по всей Украине и миру</SubTitle>
         <MainText>
           Вы можете посмотреть возможные способы и стоимость доставки в вашем
           городе или в вашей стране
         </MainText>
-      </Delivery>
-      <Availability>
+        </div>
+      </li>
+      <li className = {css.list}>
+          <div className={css.avaliability}>
         <SubTitle>Уточнить наличие</SubTitle>
         <MainText>
           На нашем сайте вы всегда можете посмотреть нличие товара в магазинах
         </MainText>
-      </Availability>
-      <Product>
+        </div>
+      </li>
+      <li className = {css.list}>
+          <div className={css.product}>
         <SubTitle>Сообщить о поступлении</SubTitle>
         <MainText>
           Если нужного товара временно нет в наличии, вы можете воспользовать
           функцией оповещения
         </MainText>
-      </Product>
-      <Provider>
+        </div>
+      </li>
+      <li className = {css.list}>
+          <div className={css.provider}>
         <SubTitle>Юридическим лицам</SubTitle>
         <MainText>Мы зарегестрированы на портале поставщиков</MainText>
-      </Provider>
-    </Section>
+        </div>
+      </li>
+    </ul>
   );
 };
 
-const Section = styled.section`
-  position: relative;
-  @media screen and (min-width: 768px) {
-    padding-top: 30px;
-    width: 100%;
-    margin: 0 auto;
-    display: flex;
-    flex-wrap: wrap;
-    align-content: center;
-    justify-content: space-evenly;
-  }
-`;
+// const Section = styled.section`
+//   position: relative;
+//   @media screen and (min-width: 768px) {
+//     padding-top: 30px;
+//     width: 100%;
+//     margin: 0 auto;
+//     display: flex;
+//     flex-wrap: wrap;
+//     align-content: center;
+//     justify-content: space-evenly;
+//   }
+// `;
 
 const SubTitle = styled.h4`
   font-family: "Montserrat", sans-serif;
@@ -64,59 +72,6 @@ const MainText = styled.p`
   color: #999999;
 `;
 
-const Delivery = styled.div`
-  width: 210px;
-  &:before {
-    display: block;
-    width: 52px;
-    height: 51px;
-    content: "";
-    background-image: url("../../assets/icon/truck.svg");
-    position: absolute;
-    left: 0px;
-    right: 50%;
-  }
-`;
 
-const Availability = styled.div`
-  width: 210px;
-  &:before {
-    display: block;
-    width: 52px;
-    height: 51px;
-    content: "";
-    background-image: url("../../assets/icon/bars.svg");
-    position: absolute;
-    left: 0px;
-    right: 50%;
-  }
-`;
 
-const Product = styled.div`
-  width: 210px;
-  &:before {
-    display: block;
-    width: 52px;
-    height: 51px;
-    content: "";
-    background-image: url("../../assets/icon/sent.svg");
-    position: absolute;
-    left: 0px;
-    right: 50%;
-  }
-`;
-
-const Provider = styled.div`
-  width: 210px;
-  &:before {
-    display: block;
-    width: 52px;
-    height: 51px;
-    content: "";
-    background-image: url("../../assets/icon/sketch.svg");
-    position: absolute;
-    left: 0px;
-    right: 50%;
-  }
-`;
 export default Advantages;
